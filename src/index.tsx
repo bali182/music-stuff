e91c4d2514dfc8479928c1efe9dd2364865c74d2
Édes Balázs <edesbalazs@gmail.com>
2019-01-12 20:58:34 +0000

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Scale } from 'tonal'
+import { triads, chords } from 'tonal-key'
+import Fretboard from 'react-fretboard'
+import { names } from 'tonal-chord'
 
-render(<div>Hi</div>, document.getElementById('root'))
+render(
+  <div style={{ fontFamily: 'sans-serif' }}>
+    <Fretboard
+      skinType="strings"
+      noteType="pc"
+      showNotes={true}
+      showSelectionLabels={true}
+      nrOfFrets={22}
+      selectedNotes={Scale.notes('G', 'mixolydian')}
+    />
+  </div>,
+  document.getElementById('root')
+)
 
-const w = window as any
+console.log(Scale.notes('G', 'mixolydian pentatonic'))
+console.log(Scale.names())
